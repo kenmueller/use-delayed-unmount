0dd793a8f777e197dc61084535642b3c6973a6d3
Mihai Cernusca <mcernusca@gmail.com>
2023-03-18 15:25:38 -0400

    Support interruptions and fix flashing behavior

--- src/index.js
+++ src/index.js
@@ -1,26 +1,52 @@
-const { useState, useCallback } = require('react')
+const { useState, useCallback, useRef } = require("react");
 
-const sleep = timeout =>
-	new Promise(resolve => setTimeout(resolve, timeout))
+const useDelayedUnmount = (isInitiallyMounted, delay = 300) => {
+	const [state, setState] = useState({
+		isMounted: isInitiallyMounted,
+		isUnmounting: false,
+	});
+	const timeoutRef = useRef(null);
 
-const useDelayedUnmount = (isInitiallyMounted, delay) => {
-	const [isMounted, _setIsMounted] = useState(isInitiallyMounted)
-	const [isUnmounting, setIsUnmounting] = useState(false)
-	
-	const setIsMounted = useCallback(async _isMounted => {
-		if (isMounted === _isMounted)
-			return
-		
-		if (!_isMounted) {
-			setIsUnmounting(true)
-			await sleep(delay)
-			setIsUnmounting(false)
-		}
-		
-		_setIsMounted(_isMounted)
-	}, [isMounted, delay, _setIsMounted, setIsUnmounting])
-	
-	return [[isMounted, isUnmounting], setIsMounted]
-}
+	const setIsMounted = useCallback(
+		(_isMounted) => {
+			// Since we delay setting the mount state we compute the logical "open" state:
+			const isOpen = state.isUnmounting ? false : state.isMounted;
 
-module.exports = useDelayedUnmount
+			// Support useState callback pattern
+			if (typeof _isMounted === "function") {
+				_isMounted = _isMounted(isOpen);
+			}
+
+			if (isOpen === _isMounted) {
+				return;
+			}
+
+			if (_isMounted) {
+				if (timeoutRef.current) {
+					clearTimeout(timeoutRef.current);
+					timeoutRef.current = null;
+				}
+				setState({
+					isMounted: true,
+					isUnmounting: false,
+				});
+			} else {
+				setState({
+					isMounted: true,
+					isUnmounting: true,
+				});
+				timeoutRef.current = setTimeout(() => {
+					setState({
+						isMounted: false,
+						isUnmounting: false,
+					});
+					timeoutRef.current = null;
+				}, delay);
+			}
+		},
+		[state, setState, delay]
+	);
+	return [[state.isMounted, state.isUnmounting], setIsMounted];
+};
+
+module.exports = useDelayedUnmount;
